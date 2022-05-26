8bde47e159a4376b360a5fad2ef205e40af19c1d
YanLong-111 <2944972991@qq.com>
2022-05-26 19:31:12 +0800

    update AttemptPermissionControlCrack contract

--- contracts/echidna/AttemptPermissionControlCrack.sol
+++ contracts/echidna/AttemptPermissionControlCrack.sol
@@ -1,46 +1,65 @@
-contract Test {
-    struct RoleData {
-        mapping(address => bool) members;
-        bytes32 adminRole;
-    }
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
 
-    bool private flag0 = true;
-    mapping(bytes32 => RoleData) private _roles;
-    bytes32 public constant MONEY_ADMINISTRATOR = bytes32(keccak256(abi.encodePacked("MFI_Money_Administrator")));
-    bytes32 public constant DATA_ADMINISTRATOR = bytes32(keccak256(abi.encodePacked("MFI_Data_Administrator")));
-    bytes32 public constant PROJECT_ADMINISTRATOR = bytes32(keccak256(abi.encodePacked("MFI_Project_Administrator")));
-    bytes32 public constant META_FINANCE_TRIGGER_POOL = bytes32(keccak256(abi.encodePacked("META_FINANCE_TRIGGER_POOL")));
-    bytes32 public constant DEFAULT_ADMIN_ROLE = 0x00;
+import "./../utils/MfiAccessControl.sol";
+
+contract AttemptPermissionControlCrack is MfiAccessControl {
+
+    bool private MONEY_ADMINISTRATORFlag0 = true;
+    bool private DATA_ADMINISTRATORFlag0 = true;
+    bool private PROJECT_ADMINISTRATORFlag0 = true;
+    bool private META_FINANCE_TRIGGER_POOLFlag0 = true;
+    bool private DEFAULT_ADMIN_ROLEFlag0 = true;
 
     event Flag(bool);
 
     function echidna_crack_MONEY_ADMINISTRATOR() public returns (bool){
-        emit Flag(flag0);
-        return flag0;
+        emit Flag(MONEY_ADMINISTRATORFlag0);
+        return MONEY_ADMINISTRATORFlag0;
     }
 
     function echidna_crack_DATA_ADMINISTRATOR() public returns (bool){
-        emit Flag(flag0);
-        return flag0;
+        emit Flag(DATA_ADMINISTRATORFlag0);
+        return DATA_ADMINISTRATORFlag0;
     }
 
     function echidna_crack_PROJECT_ADMINISTRATOR() public returns (bool){
-        emit Flag(flag0);
-        return flag0;
+        emit Flag(PROJECT_ADMINISTRATORFlag0);
+        return PROJECT_ADMINISTRATORFlag0;
     }
 
     function echidna_crack_META_FINANCE_TRIGGER_POOL() public returns (bool){
-        emit Flag(flag0);
-        return flag0;
+        emit Flag(META_FINANCE_TRIGGER_POOLFlag0);
+        return META_FINANCE_TRIGGER_POOLFlag0;
     }
 
     function echidna_crack_DEFAULT_ADMIN_ROLE() public returns (bool){
-        emit Flag(flag0);
-        return flag0;
+        emit Flag(DEFAULT_ADMIN_ROLEFlag0);
+        return DEFAULT_ADMIN_ROLEFlag0;
     }
 
-    function set0(address val) public {
-        if (_roles[DEFAULT_ADMIN_ROLE].members[val])
-            flag0 = false;
+    function setMONEY_ADMINISTRATOR(address val) public {
+        if (hasRole(MONEY_ADMINISTRATOR, val))
+            MONEY_ADMINISTRATORFlag0 = false;
+    }
+
+    function setDATA_ADMINISTRATOR(address val) public {
+        if (hasRole(DATA_ADMINISTRATOR, val))
+            DATA_ADMINISTRATORFlag0 = false;
+    }
+
+    function setPROJECT_ADMINISTRATOR(address val) public {
+        if (hasRole(PROJECT_ADMINISTRATOR, val))
+            PROJECT_ADMINISTRATORFlag0 = false;
+    }
+
+    function setMETA_FINANCE_TRIGGER_POOL(address val) public {
+        if (hasRole(META_FINANCE_TRIGGER_POOL, val))
+            META_FINANCE_TRIGGER_POOLFlag0 = false;
+    }
+
+    function setDEFAULT_ADMIN_ROLE(address val) public {
+        if (hasRole(DEFAULT_ADMIN_ROLE, val))
+            DEFAULT_ADMIN_ROLEFlag0 = false;
     }
 }
